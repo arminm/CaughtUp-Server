dc1965302a0300b793f6e8c751d0c9ebaa28d494
Dimitris Sarlis <sarlis.dim@gmail.com>
2016-04-15 15:32:25 -0700

    Added socket package

---
+++ src/main/java/news/caughtup/socket/Dispatcher.java
@@ -0,0 +1,40 @@
+package news.caughtup.socket;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dispatcher {
+	private Map<String, CaughtUpSocket> caughtUpSockets;
+	private static Dispatcher dispatcher;
+	private SocketMessage socketMessage;
+	
+	private Dispatcher() {}
+	
+	public Dispatcher getDispatcher() {
+		if (dispatcher == null) {
+			dispatcher = new Dispatcher();
+			caughtUpSockets = new HashMap<>();
+		}
+		return dispatcher;
+	}
+
+	public SocketMessage getSocketMessage() {
+		return socketMessage;
+	}
+
+	public void setSocketMessage(SocketMessage socketMessage) {
+		this.socketMessage = socketMessage;
+	}
+	
+	public void attach(String username, CaughtUpSocket socket) {
+		if (!caughtUpSockets.containsKey(username)) {
+			caughtUpSockets.put(username, socket);
+		}
+	}
+	
+	public void broadcast() {
+		for (String key: caughtUpSockets.keySet()) {
+			caughtUpSockets.get(key).emit(socketMessage);
+		}
+	}
+}
