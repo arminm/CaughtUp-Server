1a71dc816c032f7ceeee22c6bf51a9c9b46868c4
Dimitris Sarlis <sarlis.dim@gmail.com>
2016-04-20 17:36:53 -0700

    Added password model for logging in a user

--- src/main/java/news/caughtup/rest/LoginServlet.java
+++ src/main/java/news/caughtup/rest/LoginServlet.java
@@ -8,13 +8,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
+import news.caughtup.model.Password;
+
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getRequestURI().substring(req.getContextPath().length()).split("/")[2];
+        Gson gson = new Gson();
+        StringBuilder sb = new StringBuilder();
+        String s;
+        while ((s = req.getReader().readLine()) != null) {
+            sb.append(s);
+        }
+        Password password = (Password) gson.fromJson(sb.toString(), Password.class);
+        
         PrintWriter out = resp.getWriter();
-        out.println("Successfully logged in user: " + username);
+        out.println("Successfully logged in user: " + username + " password: " + password.toString());
     }
 }
