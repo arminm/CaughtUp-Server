16f6ad3ee95cfb2bc578323ce18c505160af9adc
Dimitris Sarlis <sarlis.dim@gmail.com>
2016-04-24 17:48:51 -0700

    Picture upload, download and delete on S3

--- src/main/java/news/caughtup/s3/PictureClient.java
+++ src/main/java/news/caughtup/s3/PictureClient.java
@@ -1,5 +1,9 @@
 package news.caughtup.s3;
 
+import java.io.InputStream;
+
 public interface PictureClient {
-    public boolean uploadPicture();
+    public String uploadPicture(InputStream stream);
+    public InputStream downloadPicture();
+    public boolean deletePicture();
 }
--- src/main/java/news/caughtup/s3/S3Proxy.java
+++ src/main/java/news/caughtup/s3/S3Proxy.java
@@ -1,18 +1,26 @@
 package news.caughtup.s3;
 
+import java.io.InputStream;
+
 public class S3Proxy implements PictureClient {
     private S3Client s3Client;
-    private String pictureURL;
 
-    public S3Proxy(String pictureURL) {
-        this.pictureURL = pictureURL;
+    public S3Proxy(String bucketName, String pictureName) {
+        s3Client = new S3Client(bucketName, pictureName);
     }
 
     @Override
-    public boolean uploadPicture() {
-        if (s3Client == null) {
-            s3Client = new S3Client(pictureURL);
-        }
-        return s3Client.uploadPicture();
+    public String uploadPicture(InputStream stream) {
+        return s3Client.uploadPicture(stream);
+    }
+
+    @Override
+    public InputStream downloadPicture() {
+        return s3Client.downloadPicture();
+    }
+
+    @Override
+    public boolean deletePicture() {
+        return s3Client.deletePicture();
     }	
 }
