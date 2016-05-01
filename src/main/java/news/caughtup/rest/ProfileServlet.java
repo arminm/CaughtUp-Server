3a7527bc6f0192c09129f79aa4c376759c7fb6cc
Dimitris Sarlis <sarlis.dim@gmail.com>
2016-04-30 19:21:13 -0700

    Getting the profile info of a user

--- src/main/java/news/caughtup/rest/ProfileServlet.java
+++ src/main/java/news/caughtup/rest/ProfileServlet.java
@@ -2,20 +2,50 @@ package news.caughtup.rest;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import news.caughtup.database.UserDBAdapter;
+import news.caughtup.model.User;
+import news.caughtup.util.Helpers;
+
 public class ProfileServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    /**
+     * [GET] /profile/:username
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getRequestURI().substring(req.getContextPath().length()).split("/")[2];
+     // Prepare to send JSON response back to the client
+        resp.setContentType("application/json");
         PrintWriter out = resp.getWriter();
-        out.println("Successfully got profile of user: " + username);
+        
+        String[] substrings = req.getRequestURI().substring(req.getContextPath().length()).split("/");
+        String username = substrings[substrings.length - 1];
+        
+        // Get user from DB
+        User existingUser = null;
+        try {
+            existingUser = UserDBAdapter.getUser(username);
+
+            // Send JSON response back to the client
+            if (existingUser == null) {
+                resp.setStatus(404);
+                out.println(Helpers.getErrorJSON("Internal error"));
+            } else {
+                out.println(Helpers.getGson().toJson(existingUser));
+            }
+        } catch (SQLException e) {
+            System.err.println("Failed to get user with username: " + username);
+            System.err.println(e);
+            resp.setStatus(500);
+            out.println(Helpers.getErrorJSON("Internal Error."));
+        }
     }
 
     @Override
@@ -26,6 +56,7 @@ public class ProfileServlet extends HttpServlet {
         if (isPicture.equals("true")) {
             out.println("Successfully updated profile picture of user: " + username);
         } else {
+            
             out.println("Successfully updated profile info of user: " + username);
         }
     }
